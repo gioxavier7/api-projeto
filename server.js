e957cdffa6ce04d950cb6de983585e6c19cb4d0e
gioxavier7 <giovanna.gsx1997@gmail.com>
2024-11-17 11:50:25 -0300

    preparando o ambiente do projeto (imports, etc)

---
+++ server.js
@@ -0,0 +1,17 @@
+/*
+    -> Objetivo:
+                Criar uma API de Usuários:
+                - Criar um usuário
+                - Listar todos os usuários
+                - Editar um usuário
+                - Deletar um usuário
+
+    -> Data: 17/11/2024
+    -> Versão: 1.0
+    -> Desenvolvedor: Giovanna Xavier
+*/
+
+import express from 'express'
+
+const app = express()
+
